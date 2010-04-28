430420f745c6319585de580ee5b1cca7e0b6b35e
Alexis Bienvenüe <paamc@passoire.fr>
2010-04-28 08:10:08 +0000

    Petites modifs pour Mandriva/KDE

--- local/versions.pl
+++ local/versions.pl
@@ -1,6 +1,44 @@
 #! /usr/bin/perl
+#
+# Copyright (C) 2008-2010 Alexis Bienvenue <paamc@passoire.fr>
+#
+# This file is part of Auto-Multiple-Choice
+#
+# Auto-Multiple-Choice is free software: you can redistribute it
+# and/or modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation, either version 3 of
+# the License, or (at your option) any later version.
+#
+# Auto-Multiple-Choice is distributed in the hope that it will be
+# useful, but WITHOUT ANY WARRANTY; without even the implied warranty
+# of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Auto-Multiple-Choice.  If not, see
+# <http://www.gnu.org/licenses/>.
 
 use Data::Dumper;
+
+my $lic_head=q!# Copyright (C) 2008-2010 Alexis Bienvenue <paamc@passoire.fr>
+#
+# This file is part of Auto-Multiple-Choice
+#
+# Auto-Multiple-Choice is free software: you can redistribute it
+# and/or modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation, either version 3 of
+# the License, or (at your option) any later version.
+#
+# Auto-Multiple-Choice is distributed in the hope that it will be
+# useful, but WITHOUT ANY WARRANTY; without even the implied warranty
+# of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Auto-Multiple-Choice.  If not, see
+# <http://www.gnu.org/licenses/>.
+
+!;
 
 my %k=();
 
@@ -20,10 +58,12 @@ LINES: while(<CHL>) {
 $d = Data::Dumper->new([\%k], ['k']); 
 
 open(VPL,">nv.pl");
+print VPL $lic_head;
 print VPL $d->Dump;
 close(VPL);
 
 open(VMK,">Makefile.versions");
+print VMK $lic_head;
 print VMK "PACKAGE_V_DEB=$k{'deb'}\n";
 print VMK "PACKAGE_V_SVN=$k{'svn'}\n";
 close(VMK);
