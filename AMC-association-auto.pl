5ca8b506dd648e8b535f78a0a66d01aecd7fb829
Alexis Bienvenüe <paamc@passoire.fr>
2012-01-27 13:58:34 +0000

    Nettoyage des dependances de paquets superflues de AMC-association-auto

--- AMC-association-auto.pl
+++ AMC-association-auto.pl
@@ -1,6 +1,6 @@
 #! /usr/bin/perl
 #
-# Copyright (C) 2009-2011 Alexis Bienvenue <paamc@passoire.fr>
+# Copyright (C) 2009-2012 Alexis Bienvenue <paamc@passoire.fr>
 #
 # This file is part of Auto-Multiple-Choice
 #
@@ -19,11 +19,9 @@
 # <http://www.gnu.org/licenses/>.
 
 use Getopt::Long;
-use XML::Simple;
 use AMC::Basic;
 use AMC::NamesFile;
 use AMC::Data;
-use Data::Dumper;
 
 my $notes_id='';
 my $liste_file='';
