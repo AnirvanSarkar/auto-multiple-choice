5d04ace46447848e8970cb3d091cd6b3c6cc7345
Alexis Bienvenüe <paamc@passoire.fr>
2017-12-26 16:59:35 +0100

    Bugfix: when using 0;0;1 style ticked boxes in CSV export, don't forget the enclosing "". Refs #520.

--- AMC-perl/AMC/Export/CSV.pm
+++ AMC-perl/AMC/Export/CSV.pm
@@ -131,8 +131,10 @@ sub export {
 	push @columns,$self->parse_num($self->{'_scoring'}->question_score(@sc,$q->{'question'}));
 	if($self->{'out.ticked'}) {
 	  if($self->{'out.ticked'} eq '01') {
-	    push @columns,join(';',$self->{'_capture'}
-			       ->ticked_list_0(@sc,$q->{'question'},$dt,$dtu));
+	    push @columns,
+              $self->parse_string
+              (join(';',$self->{'_capture'}
+                    ->ticked_list_0(@sc,$q->{'question'},$dt,$dtu)));
 	  } elsif($self->{'out.ticked'} eq 'AB') {
 	    my $t='';
 	    my @tl=$self->{'_capture'}
@@ -145,9 +147,9 @@ sub export {
 	    for my $i (0..$#tl) {
 	      $t.=chr(ord('A')+$i) if($tl[$i]);
 	    }
-	    push @columns,"\"$t\"";
+	    push @columns,$self->parse_string($t);
 	  } else {
-	    push @columns,'"S?"';
+	    push @columns,$self->parse_string('S?');
 	  }
 	}
       }
