4ca39bbf03488828a87e05a873379ae39cdce6cf
Alexis Bienvenüe <paamc@passoire.fr>
2018-05-30 15:43:37 +0200

    GUI: stores position and dimensions of the windows, and check that they don't exceed the screen dimensions before restoring them.

--- AMC-perl/AMC/Gui/WindowSize.pm
+++ AMC-perl/AMC/Gui/WindowSize.pm
@@ -20,22 +20,49 @@
 
 package AMC::Gui::WindowSize;
 
-sub size_monitor {
-  my ($window,$options)=@_;
-  if($options->{config}) {
-    if($options->{config}->get($options->{key}) =~ /^([0-9]+)x([0-9]+)$/) {
-      $window->resize($1,$2);
+sub constraints {
+  my ($x0, $dx, $dxmax)=@_;
+  my $over = $$x0 + $$dx - $dxmax;
+  if($over > 0) {
+    if($over > $$x0) {
+      $$x0 = 0;
+      $$dx = $dxmax;
+    } else {
+      $$x0 -= $over;
     }
-    $window->signal_connect('configure-event'=>\&AMC::Gui::WindowSize::resize,
+  }
+}
+
+sub size_monitor {
+  my ($window, $options)=@_;
+  if($options->{config}) {
+    if($options->{config}->get($options->{key})
+       =~ /^([0-9]+)x([0-9]+)(?:\+([0-9]+)\+([0-9]+))?$/) {
+      my $target_w = $1;
+      my $target_h = $2;
+      my $x = $3; $x = 0 if(!defined($x));
+      my $y = $4; $y = 0 if(!defined($y));
+      my $screen = $window->get_screen();
+      my $max_w = $screen->get_width;
+      my $max_h = $screen->get_height;
+
+      constraints(\$x, \$target_w, $max_w);
+      constraints(\$y, \$target_h, $max_h);
+
+      $window->move($x, $y);
+      $window->resize($target_w, $target_h);
+    }
+    $window->signal_connect('configure-event' => \&AMC::Gui::WindowSize::resize,
 			    $options);
   }
 }
 
 sub resize {
-  my ($window,$event,$options)=@_;
+  my ($window, $event, $options)=@_;
   if($options->{config} && $event->type eq 'configure') {
-    my $dims=join('x',$event->width,$event->height);
-    $options->{config}->set($options->{key},$dims);
+    my $dims=join('x', $event->width, $event->height);
+    my $pos=join('+', $event->window->get_root_origin);
+    $options->{config}->set($options->{key}, $dims . "+" . $pos);
   }
   0;
 }
