ae175d1573298fbc8cd4189cd8b04cba4f9b86fe
Alexis Bienvenüe <paamc@passoire.fr>
2020-12-14 16:18:29 +0100

    Fix survey test

--- tests/t/survey/test.pl
+++ tests/t/survey/test.pl
@@ -18,14 +18,30 @@
 # along with Auto-Multiple-Choice.  If not, see
 # <http://www.gnu.org/licenses/>.
 
-#require "./AMC/Test.pm";
+require "./AMC/Test.pm";
 
-#AMC::Test->new(
-#    dir             => __FILE__,
-#    tex_engine      => 'pdflatex',
-#    seuil           => 0.5,
-#    perfect_copy    => '',
-#    ignore_red      => 1,
-#    export_full_csv => [ { -copy => 4, -question => 'ballot1', -abc => 'BE' } ],
-#)->default_process;
+AMC::Test->new(
+    dir             => __FILE__,
+    tex_engine      => 'pdflatex',
+    seuil           => 0.5,
+    perfect_copy    => '',
+    export_full_csv => [
+        { -copy => 1, -question => 'capitalist', -abc   => 'A' },
+        { -copy => 1, -question => 'children',   -abc   => 'E' },
+        { -copy => 1, -question => 'local',      -abc   => 'B' },
+        { -copy => 1, -question => 'existence',  -abc   => 'A' },
+        { -copy => 1, -question => 'safety1',    -score => 99 },
+        { -copy => 1, -question => 'safety2',    -score => 1 },
+        { -copy => 1, -question => 'safety3',    -score => 1 },
+        { -copy => 1, -question => 'hyg1',       -score => 4 },
+        { -copy => 1, -question => 'hyg2',       -score => 3 },
+        { -copy => 1, -question => 'hyg3',       -score => 4 },
+        { -copy => 1, -question => 'hyg4',       -score => 5 },
+        { -copy => 1, -question => 'hyg5',       -score => 2 },
+        { -copy => 1, -question => 'hyg6',       -score => 2 },
+        { -copy => 1, -question => 'hyg7',       -score => 1 },
+        { -copy => 1, -question => 'hyg8',       -score => 5 },
+        { -copy => 1, -question => 'hyg9',       -score => 4 },
+    ],
+)->default_process;
 
