2ed02ea1292ece2aa52458e484609990d70bf043
Alexis Bienvenüe <paamc@passoire.fr>
2021-01-05 09:24:35 +0100

    Copyright year bump.

--- AMC-perl/AMC/Gui/Learning.pm
+++ AMC-perl/AMC/Gui/Learning.pm
@@ -1,6 +1,6 @@
 #! /usr/bin/perl -w
 #
-# Copyright (C) 2020 Alexis Bienvenue <paamc@passoire.fr>
+# Copyright (C) 2020-2021 Alexis Bienvenüe <paamc@passoire.fr>
 #
 # This file is part of Auto-Multiple-Choice
 #
