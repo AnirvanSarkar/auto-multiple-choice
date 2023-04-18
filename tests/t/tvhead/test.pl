06a65cd032122f71d973598fc1c10f0e5ae2994a
Alexis Bienvenüe <paamc@passoire.fr>
2023-04-18 14:27:35 +0200

    Test for AMCnumericChoice/Tvhead.

---
+++ tests/t/tvhead/test.pl
@@ -0,0 +1,29 @@
+#! /usr/bin/env perl
+#
+# Copyright (C) 2023 Alexis Bienvenüe <paamc@passoire.fr>
+#
+# This file is part of Auto-Multiple-Choice
+#
+# Auto-Multiple-Choice is free software: you can redistribute it
+# and/or modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation, either version 2 of
+# the License, or (at your option) any later version.
+#
+# Auto-Multiple-Choice is distributed in the hope that it will be
+# useful, but WITHOUT ANY WARRANTY; without even the implied warranty
+# of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Auto-Multiple-Choice.  If not, see
+# <http://www.gnu.org/licenses/>.
+
+require "./AMC/Test.pm";
+
+AMC::Test->new(
+    dir         => __FILE__,
+    seuil       => 0.1,
+    notemax     => 0,
+    check_marks => { 1 => 3 },
+)->default_process;
+
